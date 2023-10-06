bd3ce881d53c905ab469d8d11e3a57f7e4ee5906
aent <alintssr@gmail.com>
2023-10-06 02:18:58 -0400

    tried to figure out JavaScript rendering

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,10 +1,47 @@
 
         // Initialize an array with default comment objects
         const commentsArray = [
-            { name: "User1", timestamp: new Date(), text: "This is comment 1" },
-            { name: "User2", timestamp: new Date(), text: "This is comment 2" },
-            { name: "User3", timestamp: new Date(), text: "This is comment 3" }
+            { name: "Connor Walton", timestamp: "02/17/2021", text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains." },
+            { name: "Emilie Beach", timestamp: "01/09/2021", text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day." },
+            { name: "Miles Acosta", timestamp: "12/20/2020", text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough." }
         ];
+
+
+        // const commentsList = document.querySelector('.comments__list');
+        // const commentForm = document.querySelector('.comment__top--container');
+
+        // function showComment(comment){
+        //     const item = document.createElement('li');
+        //     item.innerText = `${commentsArray.name} ${commentsArray.timestamp} ${commentsArray.text}`;
+        //     commentsList.appendChild(item);
+        // }
+
+        // function clear(){
+        //     commentsList.innerText = "";
+        // }
+
+        // function addText(event){
+        //     event.preventDefault();
+
+        //     const nameDiv = document.querySelector(".comment__1");
+        //     const commentDiv = document.querySelector('.comment__2');
+
+        //     const name = nameDiv.value;
+        //     const commentText = commentDiv.value;
+        //     const timestamp = new Date().toISOString();
+
+        //     const newComment = { name, timestamp, text: commentText };
+        //     commentsArray.push(newComment);
+
+        //     // Clear input fields
+        //     nameDiv.value = "";
+        //     commentDiv.value = "";
+
+        // }
+
+        // commentForm.addEventListener("submit", addText);
+
+        // comments.forEach(displayComment);
 
         // Function to display a comment on the page
         function displayComment(comment) {
